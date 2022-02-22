d812ce85ecea7807cb3105dae682b68dca114460
Rached <r.mejri74100@gmail.com>
2022-02-22 17:12:37 +0100

    feat: impl. proof trait for proof of work

--- src/blockshain/proof.rs
+++ src/blockshain/proof.rs
@@ -1,18 +1,33 @@
+use crate::blockshain::block::Block;
+use crate::blockshain::block_hash::BlockHash;
+use std::cell::RefCell;
+use std::str::FromStr;
+use uuid::Uuid;
+
 struct ProofOfWork;
+
+/// TODO
 struct ProofOfStack;
 
 trait Proof {
-    fn proof();
+    fn proof(block: &Block) -> String;
+    fn finish(hash: &impl BlockHash) -> bool;
 }
 
 impl Proof for ProofOfWork {
-    fn proof() {
-        todo!()
-    }
-}
+    fn proof(block: &Block) -> String {
+        let proof = Uuid::new_v4().to_string();
+        let block = Block::new_with_proof(
+            Uuid::from_str(&*block.signature).unwrap(),
+            block.data.to_string(),
+            block.previous_block_hash.clone(),
+            proof,
+        );
 
-impl Proof for ProofOfStack {
-    fn proof() {
+        block.hash.as_string()
+    }
+
+    fn finish(hash: &impl BlockHash) -> bool {
         todo!()
     }
 }
